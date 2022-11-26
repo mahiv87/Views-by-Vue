df84dd605753b2debad12338e07c3226d24847d6
Marcus Herrera <maherreraiv1987@gmail.com>
2022-11-25 19:27:45 -0700

    header coded

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,44 @@ import NavVue from './components/Nav.vue';
 </script>
 
 <template>
-	<!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div> -->
-	<!-- <HelloWorld msg="Vite + Vue" /> -->
 	<NavVue />
+	<div class="heading">
+		<p class="title">Vue Your TODOs</p>
+		<p class="subtitle">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+	</div>
 </template>
 
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
+.heading {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 0px;
+	width: 992px;
+	height: 200px;
+	margin-inline: auto;
 }
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
+.subtitle {
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	padding: 0px 8px;
+	width: 500px;
+	height: 35px;
+	color: #abd1c6;
+	font-size: 29.33px;
+	text-align: center;
 }
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
+.title {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 0px;
+	margin-bottom: 0;
+	width: 981px;
+	height: 112px;
+	color: #fffffe;
+	font-size: 92.59px;
 }
 </style>
