2769afb247bae17d40f778db983262b7c90b8795
Marcus Herrera <maherreraiv1987@gmail.com>
2022-11-25 19:00:51 -0700

    initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="nav-container">
+		<img src="../assets/views-logo-transparent.svg" alt="Views logo" class="nav-logo" />
+		<div class="nav-links">
+			<p class="nav-button">GitHub</p>
+			<p class="nav-button">Donate</p>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.nav-container {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	padding: 0px 25px;
+	gap: 350px;
+	background: #001e1d;
+	height: 70px;
+}
+
+.nav-logo {
+	position: relative;
+	top: 33px;
+	width: 150px;
+	height: auto;
+}
+
+.nav-links {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	padding: 0px;
+	gap: 36px;
+	width: 274px;
+	height: 47px;
+}
+
+.nav-button {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 10px;
+	width: 75px;
+	height: 25px;
+	border-radius: 30px;
+	color: #fffffe;
+}
+
+.nav-button:hover {
+	background: #e8e4e6;
+	color: #001e1d;
+}
+</style>
